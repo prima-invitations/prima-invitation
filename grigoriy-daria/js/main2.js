d7f425b1ccfbe9ac0c5936ed27dd8ed18e49b683
Oleksandr Ilnytskyi <oleksandrilnytskyi@gmail.com>
2026-08-05 14:05:08 +0300

    fix

---
+++ grigoriy-daria/js/main2.js
@@ -0,0 +1,58 @@
+// AOS animation
+AOS.init({
+  once: true
+});
+
+document.querySelector('.registration-form').addEventListener('submit', function (event) {
+  event.preventDefault(); // Отменяем стандартную отправку формы
+
+  const form = event.target;
+  const submitButton = form.querySelector('.registration-btn');
+
+  // Получаем текущую дату и время
+  const currentDate = new Date();
+  const dateString = currentDate.toLocaleString('ru-RU', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  // Устанавливаем значение поля date
+  form.querySelector('input[name="дата"]').value = dateString;
+
+  // Делаем кнопку неактивной и меняем текст
+  submitButton.disabled = true;
+  submitButton.textContent = 'Отправка...';
+
+  // Создаем объект для отправки данных формы
+  const formData = new FormData(form);
+
+  // Отправляем данные на Google Таблицы
+  fetch(
+    'https://script.google.com/macros/s/AKfycbwE-RrsMosEAsFPYKX_anZYEFTXR8NFT4UFxDPBzQPpvummR0hVJJdd7kzDMPkxXhDJ0w/exec',
+    {
+      method: 'POST',
+      body: formData,
+    },
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      // Если все прошло успешно, перенаправляем на YouTube
+      if (data.result === 'success') {
+        window.location.href = 'https://prima-invitations.vercel.app/grigoriy-daria/thanks.html';
+      } else {
+        // Если ошибка, восстанавливаем кнопку и показываем сообщение об ошибке
+        submitButton.disabled = false;
+        submitButton.textContent = 'Отправить';
+        alert('Произошла ошибка при отправке данных');
+      }
+    })
+    .catch((error) => {
+      // В случае ошибки
+      submitButton.disabled = false;
+      submitButton.textContent = 'Отправить';
+      alert('Произошла ошибка при отправке данных');
+    });
+});
