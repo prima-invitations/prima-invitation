232361d0a596ebac18eb3f82228afd20a738492f
Oleksandr Ilnytskyi <oleksandrilnytskyi@gmail.com>
2026-08-28 08:32:31 +0300

    fix

--- grigoriy-daria/js/music1.js
+++ grigoriy-daria/js/music1.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-  const audio = new Audio('assets/music.mp3');
+  const audio = new Audio('https://prima-invitations.vercel.app/grigoriy-daria/assets/music.mp3');
   const playButton = document.querySelector('.js-play-music');
 
   playButton.addEventListener('click', function () {
